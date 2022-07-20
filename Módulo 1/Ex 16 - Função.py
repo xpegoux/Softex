fc83c54b78714b19d4de076d142a4467f1ae3415
Sullivan <netinho-sport@hotmail.com>
2022-07-19 22:42:15 -0300

    Update Ex 16 - Função.py

--- Módulo 1/Ex 16 - Função.py
+++ Módulo 1/Ex 16 - Função.py
@@ -1,3 +1,4 @@
+#Aluno: Erivaldo Barros
 '''Faça uma função calculadora de dois números com três parâmetros: os dois primeiros serão os números da operação e o terceiro será a entrada que definirá a operação a ser executada. Considera a seguinte definição:
 1. Soma
 2. Subtração
