67a952f2ea806ef70c798d8612e7fe71490415ad
Sullivan <netinho-sport@hotmail.com>
2022-07-21 19:55:24 -0300

    Exercicios Softex

--- Módulo 1/Ex 17 - Input, output.py
+++ Módulo 1/Ex 17 - Input, output.py
@@ -10,39 +10,40 @@ Digite o número para a operação correspondente e caso o usuário introduza qu
 Após a seleção, o sistema deve pedir para o usuário inserir o primeiro e segundo valor, um de cada. Depois precisa executar a operação e mostrar o resultado na tela. Quando o usuário escolher a opção “Sair”, o sistema irá parar. 
 
 É necessário que o sistema mostre as opções sempre que finalizar uma operação e mostrar o resultado.'''
-import os
-
-def Calculadora(n1, n2, op):
-    soma = n1+n2 
-    sub = n1-n2
-    mult = n1*n2
-    div = n1/n2    
-    if op == 1: 
-        return (soma)
-    elif op == 2:
-        return (sub)
-    elif op == 3:
-        return (mult)
-    elif op == 4:
-        return (div)    
-
-sair = True
-while sair == True:
-    print('\nEscolha o tipo da operação:\n[1] - Soma\n[2] - Subtração\n[3] - Multiplicação\n[4] - Divisão\n[0] - Sair')
-    operador = int(input('\nDigite -> '))
-      
-    while (operador != 0) and (operador != 1) and (operador != 2) and (operador != 3) and (operador != 4):
-        print('\n\nEssa opção [',operador,'] não existe')
+#Criando função sem parâmetros
+def Calculadora():
+    sair = True 
+    while sair == True: #iniciando loop
         print('\nEscolha o tipo da operação:\n[1] - Soma\n[2] - Subtração\n[3] - Multiplicação\n[4] - Divisão\n[0] - Sair')
-        operador = int(input('\nDigite -> '))
+        op = int(input('\nDigite -> '))
+        
+        while (op != 0) and (op != 1) and (op != 2) and (op != 3) and (op != 4): #se qualquer opção diferente das existentes, fará esse segundo loop repetiro menu
+            print('\n\nEssa opção [',op,'] não existe')
+            print('\nEscolha o tipo da operação:\n[1] - Soma\n[2] - Subtração\n[3] - Multiplicação\n[4] - Divisão\n[0] - Sair')
+            op = int(input('\nDigite -> '))
 
-    if (operador == 0):
-        break        
+        if (op == 0): #condição para interromper o programa todo (alocado dentro do primeiro while), caso a opção for 0.
+            break        
+        
+        #definindo as entradas dos números pelo usuário.
+        n1 = float(input('\nDigite o primeiro número: '))
+        n2 = float(input('Digite o segundo número: '))
+        #atribuindo operações aritméticas    
+        soma = n1+n2 
+        sub = n1-n2
+        mult = n1*n2
+        div = n1/n2
+        #condição final para apresentar o valor da operação escolhida inicialmente.
+        if op == 1: 
+            print('\nResultado da operação escolhida: ',soma)
+        elif op == 2:
+            print('\nResultado da operação escolhida: ',sub)
+        elif op == 3:
+            print('\nResultado da operação escolhida: ',mult)
+        elif op == 4:
+            print('\nResultado da operação escolhida: ',div)
 
-    nu1 = float(input('\nDigite o primeiro número: ')) 
-    nu2 = float(input('Digite o segundo número: '))
-    os.system('cls')
+#chamando a função sem passar parâmetros, tendo em vista que, toda a operação está sendo feita dentro da função, incluindo os prints.            
+Calculadora()
 
-    resultado = Calculadora(nu1, nu2, operador)
-    print('\nResultado da operação escolhida: ',resultado)
-    
+        
