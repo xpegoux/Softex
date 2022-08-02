1e3e52b1f754a9bf9f7cceb7f170f6d1324e87b2
Sullivan <netinho-sport@hotmail.com>
2022-08-02 00:32:35 -0300

    Create Ex 25 - Manipulando Strings.py

---
+++ Módulo 1/Ex 25 - Manipulando Strings.py
@@ -0,0 +1,9 @@
+#Aluno: Erivaldo Barros
+'''Estruture três códigos, os quais devem conter uma função de manipulação de string que obtenha resultado.'''
+
+x = 'Manipulando Strings'
+print('Frase original: {}'.format(x),'\n')
+
+print('Manipulando posições ->', x[12:19])
+print('Transformando todas strings em maiúsculo ->', x.upper())
+print('Transformando todas strings em minúsculo ->', x.lower())
