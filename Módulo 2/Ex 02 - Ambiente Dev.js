6f48c25b14a68feda43b342a69fc5d5d9c5a675a
Sullivan <netinho-sport@hotmail.com>
2022-08-03 19:18:19 -0300

    Create Ex 02 - Ambiente Dev.js

---
+++ Módulo 2/Ex 02 - Ambiente Dev.js
@@ -0,0 +1,5 @@
+//Aluno: Erivaldo Barros
+
+/*Minha escolha é o VScode, tanto para linguagens de programação como Python e JS, quanto para linguagens de marcação de texto/estilos como HTML e CSS.
+O motivo é simples, me adaptei melhor aos atalhos, possui versatilidade e compatibilidade. E estar no TOP 1, isso significa que este editor de código está sempre se atualizando e sempre com novidades e plugins que facilitam tanto na estética quanto no desenvolvimento.
+*/
