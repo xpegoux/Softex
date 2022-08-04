1890c41360ed3e145200d1ece503f695007c0b31
Sullivan <netinho-sport@hotmail.com>
2022-08-03 23:02:32 -0300

    Update Ex 18 - Exceptions.py

--- Módulo 1/Ex 18 - Exceptions.py
+++ Módulo 1/Ex 18 - Exceptions.py
@@ -2,8 +2,11 @@
 '''Desenvolva um programa que recebe do usuário nome completo e ano de nascimento que seja entre 1922 e 2021. A partir dessas informações, o sistema mostrará o nome do usuário e a idade que completou, ou completará, no ano atual (2022).
 Caso o usuário não digite um número ou apareça um inválido no campo do ano, o sistema informará o erro e continuará perguntando até que um valor correto seja preenchido.'''
 
+from asyncio.windows_events import NULL
+
+
 x = True
-ano = 0
+ano = NULL
 idade = 2022
 nome = input('Digite seu nome completo:\n')
 while x == True:
