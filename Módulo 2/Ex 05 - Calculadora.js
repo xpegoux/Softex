fc2c3b161818fe6f8c4ebf54ce1a390d4573a0c3
Sullivan <netinho-sport@hotmail.com>
2022-08-08 19:13:12 -0300

    Update Ex 05 - Calculadora.js

--- Módulo 2/Ex 05 - Calculadora.js
+++ Módulo 2/Ex 05 - Calculadora.js
@@ -1,1 +1,22 @@
-console.log("Hello Word")
+var num_1 = Number('Digite o primeiro número:');
+var num_2 = Number('Digite o segundo número:');
+var operador = ('Digite o operador que você deseja calcular\n[+]\n[-]\n[*]\n[/]');
+
+
+
+/*Definindo condições para apresentar o cálculo dos números
+if (operador == '+') {
+    console.log(`Soma: ${num_1 + num_2}`) //document.write é utilizado para mostrar no corpo do documento.
+} else if (operador == '-') {
+    console.log(`Subtracao: ${num_1 - num_2}`)
+} else if (operador == '*') {
+    console.log(`Multiplicação: ${num_1 * num_2}`)
+} else if (operador == '/') {
+    console.log(`Divisão: ${num_1 / num_2}`)
+    var resto = num_1 % num_2
+    if (resto != 0) {
+        document.write(`<br>Resto da divisão: ${resto}`)
+    }
+} else {
+    window.alert('Operador inválido!')
+    document.write('Você digitou um operador inválido &#x1F613, atualize a página ou pressione <strong>F5</strong> e tente novamente &#x1F609.')*/
