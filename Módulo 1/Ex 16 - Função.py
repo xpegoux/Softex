add80c48224a62f58bf851ad09ead208ce013b43
Sullivan <netinho-sport@hotmail.com>
2022-08-20 00:16:58 -0300

    Update Ex 16 - Função.py

--- Módulo 1/Ex 16 - Função.py
+++ Módulo 1/Ex 16 - Função.py
@@ -7,7 +7,7 @@
 
 Caso seja inserido um número de operação que não exista, o resultado deverá ser 0.'''
 #Criando função com os três parâmetros solicitados
-def Calculadora(n1, n2, op):
+def calculadora(n1, n2, op):
     soma = n1+n2 #atribuindo operações aos parâmetros 1 e 2 e assim sucessivamente abaixo
     sub = n1-n2
     mult = n1*n2
@@ -30,7 +30,7 @@ operador = int(input('Digite: ')) #armazenando entrada do operador
 nu1 = float(input('\nDigite o primeiro número: ')) 
 nu2 = float(input('Digite o segundo número: '))
 
-resultado = Calculadora(nu1, nu2, operador) #chamando a função para resultado e declarando as variáveis no parâmetro.
+resultado = calculadora(nu1, nu2, operador) #chamando a função para resultado e declarando as variáveis no parâmetro.
 print('\nResultado da operação escolhida: ',resultado) #chamando o resultado
 if (operador != 1) and (operador != 2) and (operador != 3) and (operador != 4): #condição para dar uma breve mensagem referente ao retorno 0.
     print('Seu resultado foi 0, porque você escolheu uma operação inexistente :(')
