d2c90c960492829ca0da90a8826904b8a4a21d10
Verônica Silva09 <netinho-sport@hotmail.com>
2022-09-13 19:26:10 -0300

    Factory Method

--- Módulo 3/1_factory_method/Creator.ts
+++ Módulo 3/1_factory_method/Creator.ts
@@ -3,6 +3,9 @@ import { ConcrateP2 } from "./ConcrateP2";
 import { Product } from "./Product";
 
 export class Creator {
+    private static error(): Product {
+        throw new Error("Opção inválida!");
+    }
     //factory method
     public static createProduct(ram: number, hdd: number, cpu: number, type: boolean) : Product {        
         if (type === true) {
@@ -10,7 +13,7 @@ export class Creator {
         } else if (type === false) {
             return new ConcrateP2(ram, hdd, cpu);        
         } else {          
-            return type
+            return this.error();
         }
     }
 }
